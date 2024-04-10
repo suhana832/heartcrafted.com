f37b12123eee60ef1a587ae7a5d946f700ae68bf
suhana Attar <suhana786attar@gmail.com>
2024-04-10 22:29:18 +0530

    created half of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'cusive': ['Cedarville Cursive','cursive', 'sans'],
+      'mont': ['montserrat', 'sans'],
+      'normal':['EB Garamond', 'sans'],
+      'zedaya':['zedaya', 'sans'],
+      'tilt':['tilt neon', 'sans'],
+      'satisfy':['satisfy', 'sans']
+    },
+      extend: {
+        colors: {
+          vampireblack: '#050505',
+          cosmos: ' #610C27 ',
+          grullo: '#AC9C8D ',
+          desertsand: '#E3C1B4',
+          timberwolf: '#DDD9CE ',
+          alabaster: ' #EFECE9'
+        },
+      },
   },
   plugins: [],
 }
